fe884085b669de60bdfdfbd47e458920e81cffdc
JC Flores <tothenextcode@gmail.com>
2022-04-05 20:44:41 +0000

    Implemented assertArraysEqual to use eqArrays to compare two different arrays.

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  
+  return true;
+};
+
+//TEST CODE
+//assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+//assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
