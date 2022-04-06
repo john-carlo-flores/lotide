8839dfbfa64eef8824ff79e9e62c725a64b6c62b
JC Flores <tothenextcode@gmail.com>
2022-04-06 19:39:27 +0000

    Implemented middle to return middle elements of arrays w/ length > 2

---
+++ middle.js
@@ -0,0 +1,43 @@
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  
+  return true;
+};
+
+const middle = function(list) {
+  if (list.length < 3) {
+    return [];
+  }
+
+  let middle = Math.floor(list.length / 2);
+
+  if (list.length % 2 === 0) {
+    return list.slice(middle -1, middle + 1);
+  } else {
+    return list.slice(middle, middle + 1);
+  }
+};
+
+//TEST CODE
+assertArraysEqual(middle([1]),[]);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
