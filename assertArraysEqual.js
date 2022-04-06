08263f022600ea57f95661c5eb8ce6c0afbff8e5
JC Flores <tothenextcode@gmail.com>
2022-04-06 20:35:14 +0000

    Removed most of the else clauses to make code more readable

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,9 +1,9 @@
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
+    return console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
   }
+  
+  return console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
 };
 
 const eqArrays = function(array1, array2) {
--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   }
+    
+  return console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // TEST CODE
