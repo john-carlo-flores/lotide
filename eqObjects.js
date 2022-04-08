7df047978f587461c07e36b8efd6e839f91c4541
JC Flores <tothenextcode@gmail.com>
2022-04-08 15:47:45 +0000

    Fixed bug in eqObjects that just exits after first recursive check before checking rest of the object properties. Updated eqArrays based on refactored code

--- eqObjects.js
+++ eqObjects.js
@@ -17,7 +17,9 @@ const eqObjects = function(object1, object2) {
         return false;
       }
     } else if (typeof object1[key] === 'object' && object1[key] !== null){
-      return eqObjects(object1[key], object2[key]);
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
     } else if(object1[key] !== object2[key]) {
       return false;
     }
@@ -30,13 +32,17 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
-  
+
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) { 
+      if(!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    } else if(array1[i] !== array2[i]) {
       return false;
     }
   }
-  
+
   return true;
 };
 
