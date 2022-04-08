99064573eda911c36b8b02db6e3a05c676849681
JC Flores <tothenextcode@gmail.com>
2022-04-08 15:35:38 +0000

    Refactored eqObjects to recursively check nested objects when comparing

--- eqObjects.js
+++ eqObjects.js
@@ -16,7 +16,9 @@ const eqObjects = function(object1, object2) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
-    } else if (object1[key] !== object2[key]) {
+    } else if (typeof object1[key] === 'object' && object1[key] !== null){
+      return eqObjects(object1[key], object2[key]);
+    } else if(object1[key] !== object2[key]) {
       return false;
     }
   }
@@ -52,3 +54,8 @@ assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
