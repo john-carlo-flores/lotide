22c589aad2dbe5c1939033730df73b300d9a8b49
JC Flores <tothenextcode@gmail.com>
2022-04-11 17:19:09 +0000

    Installed mocha and chai packages. Reformatted headTest to use M & C. GitIgnored node_modules

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,20 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe('#head', function() {
+  it("should return 1 for [1,2,3]", function() {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("should return '5' for ['5']", function() {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("should return 5 for [5,6,7]", function() {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("should return 'Hello' for ['Hello', 'Lighthouse', 'Labs']", function() {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+});
