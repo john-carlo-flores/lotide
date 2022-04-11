355f953a6b13f6f90509e916312ccad1cd10fbfa
JC Flores <tothenextcode@gmail.com>
2022-04-11 16:44:09 +0000

    Refactored middle and moved test code to test folder. Exported middle

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
-  }
-
-  return console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  
-  return true;
-};
-
 const middle = function(list) {
   if (list.length < 3) {
     return [];
@@ -34,3 +12,5 @@ const middle = function(list) {
   return list.slice(middle, middle + 1);
 
 };
+
+module.exports = middle;
