1e663463721048c63bb350c59402894053cacbc0
JC Flores <tothenextcode@gmail.com>
2022-04-11 19:48:05 +0000

    Refactored asserts to properly require based on new folder structure

--- assert/assertObjectsEqual.js
+++ assert/assertObjectsEqual.js
@@ -1,34 +1,5 @@
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  }
-
-  for (const key in object1) {
-    if (Array.isArray(object1[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
-    } else if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  
-  return true;
-};
+const eqArrays = require('../lib/eqArrays');
+const eqObjects = require('../lib/eqObjects');
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
