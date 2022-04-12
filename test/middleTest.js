8f01f8fc4898501aab89139191e26c05826d520d
JC Flores <tothenextcode@gmail.com>
2022-04-12 17:39:47 +0000

    Refactored middleTests to use M & C

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,30 @@
-const assertArraysEqual = require('../assert/assertArraysEqual');
+const assertArraysEqual = require('chai').assert;
+const { assert } = require('chai');
 const middle = require('../lib/middle');
 
-//TEST CODE
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe('#middle', () => {
+  it('should return empty array when array given is empty', () => {
+    const emptyArray = [];
+    assert.strictEqual(middle(emptyArray).length, 0)
+  });
+  
+  it('should return empty array when array given has one element', () => {
+    const array = [1];
+    assert.strictEqual(middle(array).length, 0);
+  });
+  
+  it('should return empty array when array given has two elements', () => {
+    const array = [1, 2];
+    assert.strictEqual(middle(array).length, 0);
+  });
+
+  it('should return array with 2 middle elements when array given length is even', () => {
+    const array = [1, 2, 3, 4];
+    assertArraysEqual(middle(array), [2, 3]);
+  });
+
+  it('should return array with 1 middle element when array given length is odd', () => {
+    const array = [1, 2, 3, 4, 5];
+    assertArraysEqual(middle(array), [3]);
+  });
+});
