bb5697dbf8d12a6e2d3306d217f8048963218ce3
JC Flores <tothenextcode@gmail.com>
2022-04-13 17:06:47 +0000

    Added rest of the functions in lib that have not been referenced yet

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+const countLetters = require('./lib/countLetters');
+const countOnly = require('./lib/countOnly');
+const eqArrays = require('./lib/eqArrays');
+const eqObjects = require('./lib/eqObjects');
+const findKey = require('./lib/findKey');
+const findKeyByValue = require('./lib/findKeyByValue');
+const flatten = require('./lib/flatten');
 const head = require('./lib/head');
+const letterPositions = require('./lib/letterPositions');
+const map = require('./lib/map');
 const middle = require('./lib/middle');
 const tail = require('./lib/tail');
+const takeUntil = require('./lib/takeUntil');
+const without = require('./lib/without');
 
 module.exports = {
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
   head,
+  letterPositions,
+  map,
   middle,
-  tail
+  tail,
+  takeUntil,
+  without
 };
